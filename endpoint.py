08f8c4744a467b3136777e2c9b4c6aeb77bca2b6
cd <cd.skogsberg@gmail.com>
2021-11-01 12:44:05 +0100

    More fixin'

--- endpoint.py
+++ endpoint.py
@@ -7,4 +7,4 @@ hostname = socket.getfqdn()
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     return_string = "Hello World from " + hostname
-    return [return_string]
+    return return_string
