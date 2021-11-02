59bb9dae34fe08fb76ad27a63698cbc91bad2960
cd <cd.skogsberg@gmail.com>
2021-11-02 15:15:44 +0100

    yes

--- endpoint.py
+++ endpoint.py
@@ -7,7 +7,6 @@ hostname = socket.getfqdn()
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    print(env)
     if env['REQUEST_URI'] == '/api/endpoint.json':
         return_string = 'Endpoint.json requested'
     elif env['REQUEST_URI'] == '/api/login.json':
