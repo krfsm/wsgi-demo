ba1dfca3086c516023bd98626e2ef3d19299b01f
cd <cd.skogsberg@gmail.com>
2021-11-02 13:30:16 +0100

    got it up 'n' running! maybe.

--- endpoint.py
+++ endpoint.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
+import json
 import socket
 
 hostname = socket.getfqdn()
 
 def application(env, start_response):
-    print(env)
     start_response('200 OK', [('Content-Type','text/html')])
-    return_string = "Hello World from " + hostname
+    print(env)
+    if env['REQUEST_URI'] == '/api/endpoint.json':
+        return_string = 'Endpoint.json requested'
+    elif env['REQUEST_URI'] == '/api/login.json':
+        return_string = 'This will be a login endpoint'
+    else:
+        return_string = "Hello World from " + hostname
     return return_string.encode('utf-8')
