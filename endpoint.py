5fec1ce83b3804ad1caf00096b06d377d40f523b
cd <cd.skogsberg@gmail.com>
2022-02-04 21:56:58 +0100

    more flumph

--- endpoint.py
+++ endpoint.py
@@ -7,7 +7,7 @@ hostname = socket.getfqdn()
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    if env['REQUEST_URI'] == '/api/getinspiration.json':
+    if env['REQUEST_URI'] == '/api/getinspired.json':
         return_string = 'getinspiration.json requested'
     else:
         return_string = "Hello World from " + hostname
